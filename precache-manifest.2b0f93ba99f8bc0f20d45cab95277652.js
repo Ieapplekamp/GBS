8710f99e1081f2157863c685109fa5e346c26261
Ian Applekamp <ieapplekamp@gmail.com>
2020-01-08 20:50:59 -0500

    Updates

---
+++ precache-manifest.2b0f93ba99f8bc0f20d45cab95277652.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b984c22804edc6ba4490c66618d8f24c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8c994c6d7db97f1711e",
+    "url": "/static/css/main.2fa4e22b.chunk.css"
+  },
+  {
+    "revision": "33aff3ce02381ca1ec6b",
+    "url": "/static/js/2.f1b27be4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.f1b27be4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c8c994c6d7db97f1711e",
+    "url": "/static/js/main.09f6b3da.chunk.js"
+  },
+  {
+    "revision": "c6fe81ad85dcd5172146",
+    "url": "/static/js/runtime-main.38e7603d.js"
+  }
+]);
